521417b13e02304146678194d3055f5b7fb669bc
MarcusKoen <37168149+MarcusKoen@users.noreply.github.com>
2025-05-22 13:52:33 +0200

    Update main.cpp

--- Intro to pointers/main.cpp
+++ Intro to pointers/main.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int n = 5;
+    cout << n << endl;
+
+    cout << &n << endl; //address of n
+
+    int* ptr = &n;
+    cout << ptr << endl;// address of n in ptr
+
+    cout << *ptr << endl;//dereference pointer to find value of n stored in memory
+
+    *ptr = 10;
+
+    cout <<*ptr << endl;
+    cout << n << endl; //n holds 10 because address is the same
+    //pointer has to be the same type of what variable it is holding
+
+    //wrong code
+    //int* ptr2;
+   // *ptr2 = 7;//no address
+
+    //to fix
+    int v;
+    int* ptr2 = &v;
+    *ptr2 = 7;
+
+    cout << v << endl;
+
+    return 0;
+}
 

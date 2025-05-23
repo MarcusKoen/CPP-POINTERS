72a969ec0e75a31fc3f18f2502d7dd58bdd3de78
MarcusKoen <37168149+MarcusKoen@users.noreply.github.com>
2025-05-23 12:22:19 +0200

    Update main.cpp

--- Dynamic Arrays/main.cpp
+++ Dynamic Arrays/main.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int size;
+    cin >> size;
+
+    int* myArray = new int[size];//allocate mem
+
+    for (int i =0;i<size;i++)
+    {
+        cout << "Array "<< i ;
+        cin >> myArray[i];
+    }
+
+    for (int i =0;i<size;i++)
+    {
+        //cout << myArray[i] << endl;
+        cout << *(myArray + i)<< endl;
+
+    }
+
+    delete[]myArray;//weird
+    myArray = NULL;
+
+    return 0;
+}
 

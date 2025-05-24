c2b3f8cd7c9e8f8721a49238f6d5669bfb30a793
MarcusKoen <37168149+MarcusKoen@users.noreply.github.com>
2025-05-24 09:32:33 +0200

    Update main.cpp

--- Multi-Dimenstional Dynamic Arrays/main.cpp
+++ Multi-Dimenstional Dynamic Arrays/main.cpp
@@ -1,1 +1,36 @@
 
+//What a mind fuck
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int rows, cols;
+
+    cout << "How many rows, chols:" <<endl;
+    cin >> rows >>cols;
+
+        //Procedure just follow the image on github.
+    int **table = new int*[rows];
+
+    for (int i = 0; i < rows; i++)
+    {
+
+        table[i]=new int[cols];
+    }
+
+    table[1][2] = 88;
+    //Deallocate in reverse
+
+        for (int i = 0; i < rows; i++)
+    {
+
+        delete[] table[i];
+    }
+    delete[] table;
+    table = NULL;
+    return 0;
+}
+
